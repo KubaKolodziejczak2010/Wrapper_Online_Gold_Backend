6970940c4c29536191dbf8f4d644a8c1df91f8a0
KubaKolodziejczak2010 <kkolodziejczak647@gmail.com>
2024-06-20 10:56:02 +0200

    Update main.js

--- tts/main.js
+++ tts/main.js
@@ -7,74 +7,62 @@ module.exports = function (voiceName, text) {
 	return new Promise((res, rej) => {
 		const voice = voices[voiceName];
 		switch (voice.source) {
-			case 'vocalware': {
+			case "vocalware": {
+				var [eid, lid, vid] = voice.arg;
+				var cs = md5(`${eid}${lid}${vid}${text}1mp35883747uetivb9tb8108wfj`);
 				var q = qs.encode({
 					EID: voice.arg[0],
 					LID: voice.arg[1],
 					VID: voice.arg[2],
 					TXT: text,
+					EXT: "mp3",
 					IS_UTF8: 1,
-					HTTP_ERR: 1,
-					ACC: 3314795,
-					API: 2292376,
-					vwApiVersion: 2,
-					CB: 'vw_mc.vwCallback',
+					ACC: 5883747,
+					cache_flag: 3,
+					CS: cs,
 				});
-				var req = https.get({
-					host: 'cache-a.oddcast.com',
-					path: `/tts/gen.php?${q}`,
-					method: 'GET',
-					headers: {
-						Referer: 'https://www.vocalware.com/index/demo',
-						Origin: 'https://www.vocalware.com',
-						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+				var req = https.get(
+					{
+						host: "cache-a.oddcast.com",
+						path: `/tts/gen.php?${q}`,
+						headers: {
+							Referer: "https://www.oddcast.com/",
+							Origin: "https://www.oddcast.com/",
+							"User-Agent":
+								"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36",
+						},
 					},
-				}, r => {
-					var buffers = [];
-					r.on('data', d => buffers.push(d));
-					r.on('end', () => res(Buffer.concat(buffers)));
-					r.on('error', rej);
-				});
+					(r) => {
+						var buffers = [];
+						r.on("data", (d) => buffers.push(d));
+						r.on("end", () => res(Buffer.concat(buffers)));
+						r.on("error", rej);
+					}
+				);
 				break;
 			}
 			case 'voicery': {
-				var q = qs.encode({
-					text: text,
-					speaker: voice.arg,
-					ssml: text.includes('<'),
-				});
-				https.get({
-					host: 'www.voicery.com',
-					path: `/api/generate?${q}`,
-				}, r => {
-					var buffers = [];
-					r.on('data', d => buffers.push(d));
-					r.on('end', () => res(Buffer.concat(buffers)));
-					r.on('error', rej);
-				});
-				break;
+				 console.log("Voicery shut down in 2020. Voices will be removed soon.");
 			}
-			case 'watson': {
+			case "watson": {
 				var q = qs.encode({
 					text: text,
 					voice: voice.arg,
 					download: true,
 					accept: "audio/mp3",
 				});
-				console.log(https.get({
-					host: 'text-to-speech-demo.ng.bluemix.net',
-					path: `/api/v1/synthesize?${q}`,
-					headers: {
-						Referer: 'https://www.vocalware.com/index/demo',
-						Origin: 'https://www.vocalware.com',
-						'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+				https.get(
+					{
+						host: "text-to-speech-demo.ng.bluemix.net",
+						path: `/api/v1/synthesize?${q}`,
 					},
-				}, r => {
-					var buffers = [];
-					r.on('data', d => buffers.push(d));
-					r.on('end', () => res(Buffer.concat(buffers)));
-					r.on('error', rej);
-				}));
+					(r) => {
+						var buffers = [];
+						r.on("data", (d) => buffers.push(d));
+						r.on("end", () => res(Buffer.concat(buffers)));
+						r.on("error", rej);
+					}
+				);
 				break;
 			}
 		}
